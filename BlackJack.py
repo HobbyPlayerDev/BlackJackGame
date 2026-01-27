8268eba3ee6e67c41383f72eb4f984736ec7749b
HobbyPlayerDev <marcelwoz26@gmail.com>
2026-01-27 18:33:01 +0100

    Version 1.0.1
    Änderungen:
    -Das Deck ist nicht mehr als string gespeichert sondern als int
    -Ein paar Inputs die Fehler beinhalten erkennt der Code
    -Fehler das man ins Minus gehen kann behoben
    -Weitere Probleme gelöst

--- BlackJack.py
+++ BlackJack.py
@@ -4,12 +4,18 @@ import time
 Konto = 1000
 GeldGesetzt = 0
 
-Deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Bube (10)", "Dame (10)", "König (10)", "Ass (1 oder 11)"] #Alle Karten aus dem Deck
+Deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] #Bube, Dame, König werden nur als 10 Verarbeitet und angezeigt und das Ass gibt es nicht
+
 
 print("Willkommen zu BlackJack! 🃏")
 print("")
 
 def Game_function():
+    global Konto
+    global GeldGesetzt
+    global DeinErgebnis
+    global Deck
+    global WeiterZiehen
 
     print(f"Dein Kontostand liegt bei {Konto} Euro 💵")
     try:
@@ -18,7 +24,7 @@ def Game_function():
         
     except ValueError: 
         print("") 
-        print("Gib eine Gültige Zahl ein du Trottel!")
+        print("Gib eine Gültige Zahl ein.")
         print("")
         return None
 
@@ -26,24 +32,64 @@ def Game_function():
         print("Du hast nicht genug Geld little Bro 🥀") #Geld hat er aber nicht genug um die Zahl die er geschrieben hat zu zahlen
         return None
     else:
-        if GeldGesetzt == 0: #Geprüft ob es sich um mehr als 0 Euro handelt
-            print("Du musst Geld setzen.")
+        if GeldGesetzt <= 0: #Geprüft ob es sich um mehr als 0 Euro handelt
+            print("Du musst Geld setzen um zu spielen!")
             return None
         
         #Ab hier gehts normal weiter
         print("")
         print(f"Du gehst mit {GeldGesetzt} Euro in die Runde!")
+        
+        #Kartenmechanismus (nur für erste Karten des Spielers)
+        #Erste und Zweite Karte wird gezogen
+        Karte1 = random.choice(Deck)
+        Karte2 = random.choice(Deck)
+        DeinErgebnis = Karte1 + Karte2
+        print(f"Du hast eine {Karte1} und eine {Karte2} gezogen. Insgesammt hast du jetzt {DeinErgebnis}")
         print("")
-        DeineKarte = random.choice(Deck)
+
+        #Karten wurden gezogen. Möchte der Spieler weiter?
+        WeiterZiehen = input("Möchtest du noch eine Karte ziehen? ").strip().lower() #Characters sind egal #Großbuchstaben sind Egal
+        print(f"Du schreibst {WeiterZiehen}")
+        #Karte ziehen
+        def NochEineKarte():
+            global NeueKarte
+            global WeiterZiehen
+            global DeinErgebnis
+            global Konto
+            
+            if WeiterZiehen in ["ja", "weiter", "j", "hit"]:
+                print("Du ziehst eine Weitere Karte 🃏")
+                #Hier neue Karte auswürfeln
+                NeueKarte = random.choice(Deck)
+                DeinErgebnis += NeueKarte
+                print("")
+                print(f"Du hast eine {NeueKarte} gezogen. Insgesammt hast du jetzt Insgesammt: {DeinErgebnis}")
+                #Überprüfen ob man über 21 ist.
+                if DeinErgebnis > 21:
+                    print("Du bist über 21 gekommen. Du hast Verloren!")
+                    Konto -= GeldGesetzt #Geld wird genommen
+                    return None
+                else:
+                    #Man hat eine Karte genommen. Man ist unter 22.
+                    WeiterZiehen = input("Möchtest du noch eine Karte ziehen? ").strip().lower()
+                    NochEineKarte()
+            elif WeiterZiehen in ["nein", "ne", "n"]:
+                print("")
+                print(f"Du bleibst bei {DeinErgebnis}")
+                print("")
+                #Dealer hinzufügen hier! ##################
+            else:
+                #Es wird so oft Nachgefragt bis sich der Spieler für Ja oder Nein entscheidet.
+                print("Gib Ja oder Nein an!")
+                WeiterZiehen = input("Möchtest du noch eine Karte ziehen? ").strip().lower()
+                NochEineKarte()
+
+        NochEineKarte()
 
 
-
-    return None #Eigentlich kommt man hier nicht hin deswegen raus aus der Funktion
-
-
-#Ab hier gehts weiter für die "None"
-
-
+#Hier gehts für die weiter die durch sind mit der Runde ("None") Egal ob Verloren gewonnen oder abgebrochen.
+#Ab hier gehts wieder zurück in ein neues Game solange man Geld hat.
 #Du gehst immer wieder in eine Runde außer du hast kein Geld
 while True:
     if Konto <= 0:
